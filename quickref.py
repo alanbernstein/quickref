cb1fdb7367e5774c021403378bef97769cfdf490
Alan Bernstein <alanbernstein@fb.com>
2022-08-28 17:31:58 -0500

    Finish basic recursive support

--- quickref.py
+++ quickref.py
@@ -10,39 +10,42 @@ configuration is done via an environment variable:
 QR_DATA_DIR="/path/to/qr"
 this directory can contain subdirectories (partially implemented)
 
-$ qr (recursive: done)
+$ qr
   show available quickref files (in $QR_DATA_DIR)
-$ qr topic (recursive: todo)
+$ qr topic
   show all lines from all matching $QR_DATA_DIR/*/topic.txt
   a "topic" can be anything, but generally something like a language (py),
   application (blender), library/framework (django), command (git).
   also things like audio, pdf manipulation, CLI image editing.
-$ qr topic pattern (recursive: todo)
+$ qr topic pattern
   show all lines from topic.txt matching regex pattern
 $ qr topic term1 term2 ...
   show all lines from topic.txt matching all terms
-$ qr add topic "line with spaces"  (recursive: done)
+$ qr add topic "line with spaces"
   add "line with spaces" to topic.txt
-$ qr edit [topic1 [topic2 ...]]  (recursive: done)
+$ qr edit [topic1 [topic2 [subdir/topic2 ...]]]
   open specified topics in $EDITOR; this file if no topics supplied.
-$ qr alias topic shortcut  (recursive: todo)
+$ qr alias topic shortcut
   create a new alias 'shortcut' for the topic 'topic' (not yet implemented)
 """
-import sys
-import glob
-import json
-import os
-import os.path
-import subprocess
-import re
 from collections import defaultdict
 from pprint import pprint
+import glob
+import json
+import logging
+import os
+import os.path
+import re
+import subprocess
+import sys
 
 # this is kind of kludgy - at first I used filesystem soft links for this purpose,
 # but when I realized dropbox doesn't really support soft links, I stopped. I still
 # wanted to support "alias" functionality, so I added this.
 # aliases.json just contains a single json object containing KV pairs like "numpy": "py",
 # where numpy is an alias for the file $QR/py.txt
+
+log = logging.Logger('qr', level=logging.INFO)
 
 aliases = {}
 here = os.path.dirname(os.path.realpath(__file__))
@@ -88,7 +91,7 @@ def main(argv):
         # qr add topic "new line"    # add line to topic.txt
         # add line to topic.txt
         topic = expand_aliases(argv[2:3])[0]
-        append_line_to_file_multiple(topic, argv[3])
+        append_line_to_file(topic, argv[3])
 
     elif argv[1] in ['e', 'edit']:
         # qr edit [topic [topic      # edit topic.txt or qr.py
@@ -150,58 +153,55 @@ def print_tree(pth, level=1):
 
 def get_all_qr_filenames(aliases=False):
     # TODO: implement aliases=True here?
-    return glob.glob(qr_path + '/*.txt')
+    return glob.glob(qr_path + '/**/*.txt', recursive=True)
 
-def path_from_topic(topic):
-    return qr_path + '/' + topic + '.txt'
-
-def append_line_to_file_singular(topic, line):
-    # print('<append %s: `%s`>' % (qr_path+topic+'.txt', line))
-    fname = path_from_topic(topic)
-    with open(fname, 'a') as f:
-        f.write(line + '\n')
-
-def append_line_to_file_multiple(topic, line):
+def append_line_to_file(topic, line):
+    log.debug('<append %s: `%s`>' % (qr_path+topic+'.txt', line))
     files = topic_map[topic]
-    if len(files) == 0:
-        raise FileNotFoundError
+    if len(files) <= 1:
+        # if exactly one file found: append to this file
+        # if no files found: create new file
+        files = [qr_path + '/' + topic + '.txt']
     elif len(files) > 1:
         print('not adding; multiple matching topics found:')
-        # if this ends up being a problem, then support this:
-        # qr add meta/cpp "foo bar"
         for f in files:
             print('  %s' % f)
-    else:
-        with open(files[0], 'a') as f:
-            f.write(line + '\n')
+        return
+
+    with open(files[0], 'a') as f:
+        f.write(line + '\n')
 
 def open_files_for_editing(topics):
-    fnames = [os.path.realpath(__file__)]
-    if len(topics) > 0:
+    if len(topics) == 0:
+        # open this file
+        fnames = [os.path.realpath(__file__)]
+    else:
+        # open all qr files for each specified topic
         fnames = [fname for arg in topics for fname in topic_map[arg]]
+
+    if fnames == []:
+        # create a new qr file
+        fnames = [qr_path + '/' + t + '.txt' for t in topics]
 
     editor = os.getenv('EDITOR', 'undefined')
     if editor == 'undefined':
         editor = 'vim'
         print('$EDITOR not defined, using vim')
 
-    print(fnames)
     for fname in fnames:
         cmd = '%s %s' % (editor, fname)
         print(cmd)
         subprocess.call(cmd, shell=True)
-
-
+    
 def show_search_results(topic, pattern_list):
-    # print('<search %s: ' % (qr_path+topic+'.txt') + ' '.join(pattern_list) + '>')
-
+    log.debug('<show_search_results %s: ' % (topic) + ' '.join(pattern_list) + '>')
     if topic == '.':
         show_results_from_all_files(pattern_list)
         return
 
-    fname = qr_path + '/' + topic + '.txt'
-    if not os.path.isfile(fname):
-        print(topic, fname)
+    fnames = topic_map[topic]
+    if len(fnames) == 0:
+        print(topic, fnames)
         print("'%s' doesn't exist!\nsearching all qr files" % topic)
         # TODO: fuzzy match topic
         show_results_from_all_files([topic])
@@ -209,17 +209,22 @@ def show_search_results(topic, pattern_list):
         # maybe "(fname.*pattern|pattern.*fname)" ?
         return
     else:
-        res = search_file(fname, pattern_list)
-        if not res:
+        any_result = False
+        for fname in fnames:
+            res = search_file(fname, pattern_list)
+            if res:
+                any_result = True
+                print('%s\n%s' % (fname, '=' * len(fname)))
+                print(res)
+        if not any_result:
             print('no results, searching all qr files')
             # would be nice to incorporate fname into pattern too...
             # maybe "(fname.*pattern|pattern.*fname)" ??
             show_results_from_all_files(pattern_list)
-        else:
-            print(res)
 
 
 def show_results_from_all_files(pattern_list):
+    log.debug('<show_results_from_all_files>')
     fnames = get_all_qr_filenames()
     for fname in fnames:
         res = search_file(fname, pattern_list)
@@ -229,6 +234,7 @@ def show_results_from_all_files(pattern_list):
 
 
 def search_file(fname, pattern_list):
+    log.debug('<search_file %s>' % fname)
     res = []
     compiled_pattern_list = [re.compile(p, re.IGNORECASE) for p in pattern_list]
     with open(fname) as f:
